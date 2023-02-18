6264aebe342f9b1163d5119ce1656efafa6f133e
abdullah <abdullahmokbl0@gmail.com>
2023-02-18 21:06:19 +0200

    -paypal

--- pages/checkout/index.jsx
+++ pages/checkout/index.jsx
@@ -1,11 +1,11 @@
 import Checkout from '../../components/Checkout'
-import PaypalCheckoutButton from '../../components/PaypalCheckoutButton'
+// import PaypalCheckoutButton from '../../components/PaypalCheckoutButton'
 import styles from './index.module.css'
 
 export default function checkout() {
   return (
     <div className={styles.checkout}>
-      <PaypalCheckoutButton />
+      {/* <PaypalCheckoutButton /> */}
       <Checkout cart={{ price: 'price_1MbLrpB7TcbF1qgabWaY8Dy2', quantity: 1 }} />
     </div>
   )
